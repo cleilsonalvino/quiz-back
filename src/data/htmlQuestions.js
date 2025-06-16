dc150db4dcc11c3e1b309f49bb5218cc55452d65
Cleilson <137639044+cleilsonalvino@users.noreply.github.com>
2025-06-15 21:36:41 -0300

    Update htmlQuestions.js

--- src/data/htmlQuestions.js
+++ src/data/htmlQuestions.js
@@ -5,22 +5,22 @@ const htmlQuestions = [
     "id": 1,
     "descricao": "O que é HTML?",
     "alternativas": [
-      "HyperText Markup Language - Linguagem de Marcação de Hipertexto.",
       "Hyperlink Markup Language - Linguagem de Marcação de Hiperlink.",
+      "HyperTransfer Markup Language - Linguagem de Transferência de Hipertexto.",
       "HyperText Machine Language - Linguagem de Máquina Hipertexto.",
-      "HyperTransfer Markup Language - Linguagem de Transferência de Hipertexto."
+      "HyperText Markup Language - Linguagem de Marcação de Hipertexto."
     ],
     "correctAnswer": "HyperText Markup Language - Linguagem de Marcação de Hipertexto.",
-    "subject": "HTML" // Adicionando a categoria
+    "subject": "HTML"
   },
   {
     "id": 2,
     "descricao": "Qual das alternativas a seguir descreve corretamente a Web 2.0?",
     "alternativas": [
-      "Surgiu em meados de 2004, com tecnologias server-side, e é caracterizada pela colaboração e dinamismo.",
-      "Caracteriza-se pela utilização do HTML 5, permitindo uma melhor definição do conteúdo, conhecida como Web semântica.",
       "Será como um gigantesco sistema operacional inteligente, com a presença de Internet das Coisas e Inteligência Artificial.",
-      "Surgiu em 1990, com a junção de HTML, HTTP e Hyperlinks, sendo uma Web informativa e estática."
+      "Surgiu em meados de 2004, com tecnologias server-side, e é caracterizada pela colaboração e dinamismo.",
+      "Surgiu em 1990, com a junção de HTML, HTTP e Hyperlinks, sendo uma Web informativa e estática.",
+      "Caracteriza-se pela utilização do HTML 5, permitindo uma melhor definição do conteúdo, conhecida como Web semântica."
     ],
     "correctAnswer": "Surgiu em meados de 2004, com tecnologias server-side, e é caracterizada pela colaboração e dinamismo.",
     "subject": "HTML"
@@ -29,10 +29,10 @@ const htmlQuestions = [
     "id": 3,
     "descricao": "Sobre a Web 4.0, qual das afirmações é correta?",
     "alternativas": [
-      "Será como um gigantesco sistema operacional inteligente e dinâmico, utilizando dados para suportar a tomada de decisão.",
+      "Caracteriza-se pelo surgimento da World Wide Web, permitindo o compartilhamento de informações na Internet.",
       "Representa a junção de três tecnologias simples: HTML, HTTP e Hyperlinks, formando uma Web meramente informativa.",
       "É conhecida como Web semântica ou inteligente, com avanços em blockchain e machine learning.",
-      "Caracteriza-se pelo surgimento da World Wide Web, permitindo o compartilhamento de informações na Internet."
+      "Será como um gigantesco sistema operacional inteligente e dinâmico, utilizando dados para suportar a tomada de decisão."
     ],
     "correctAnswer": "Será como um gigantesco sistema operacional inteligente e dinâmico, utilizando dados para suportar a tomada de decisão.",
     "subject": "HTML"
@@ -41,10 +41,10 @@ const htmlQuestions = [
     "id": 4,
     "descricao": "Qual é a função do elemento `<head>` em um documento HTML?",
     "alternativas": [
-      "Contém informações sobre o documento, como título e links para estilos.",
-      "Define o corpo principal do conteúdo visível da página.",
       "Especifica a estrutura de dados do documento.",
-      "Cria links para outros documentos HTML."
+      "Cria links para outros documentos HTML.",
+      "Contém informações sobre o documento, como título e links para estilos.",
+      "Define o corpo principal do conteúdo visível da página."
     ],
     "correctAnswer": "Contém informações sobre o documento, como título e links para estilos.",
     "subject": "HTML"
@@ -53,10 +53,10 @@ const htmlQuestions = [
     "id": 5,
     "descricao": "O que é um elemento `<div>` em HTML?",
     "alternativas": [
-      "Um elemento de bloco usado para agrupar conteúdo.",
+      "Um elemento de mídia usado para exibir imagens.",
       "Um elemento de linha usado para exibir texto.",
-      "Um elemento de formulário usado para capturar dados do usuário.",
-      "Um elemento de mídia usado para exibir imagens."
+      "Um elemento de bloco usado para agrupar conteúdo.",
+      "Um elemento de formulário usado para capturar dados do usuário."
     ],
     "correctAnswer": "Um elemento de bloco usado para agrupar conteúdo.",
     "subject": "HTML"
@@ -65,10 +65,10 @@ const htmlQuestions = [
     "id": 6,
     "descricao": "Qual é a função do atributo `alt` em uma tag `<img>`?",
     "alternativas": [
+      "Define o estilo da imagem.",
       "Fornece uma descrição alternativa da imagem.",
       "Define o tamanho da imagem.",
-      "Especifica o link da imagem.",
-      "Define o estilo da imagem."
+      "Especifica o link da imagem."
     ],
     "correctAnswer": "Fornece uma descrição alternativa da imagem.",
     "subject": "HTML"
@@ -77,10 +77,10 @@ const htmlQuestions = [
     "id": 7,
     "descricao": "O que é um elemento `<a>` em HTML?",
     "alternativas": [
+      "Um elemento de bloco para agrupar conteúdo.",
+      "Um elemento de script para executar código JavaScript.",
       "Um link para outra página ou recurso.",
-      "Um elemento de formulário para capturar dados do usuário.",
-      "Um elemento de mídia para exibir vídeos.",
-      "Um elemento de bloco para agrupar conteúdo."
+      "Um elemento de mídia para exibir vídeos."
     ],
     "correctAnswer": "Um link para outra página ou recurso.",
     "subject": "HTML"
@@ -89,10 +89,10 @@ const htmlQuestions = [
     "id": 8,
     "descricao": "Qual é a função do atributo `href` em uma tag `<a>`?",
     "alternativas": [
-      "Especifica o destino do link.",
+      "Define a cor do link.",
       "Define o texto do link.",
       "Especifica o estilo do link.",
-      "Define a cor do link."
+      "Especifica o destino do link."
     ],
     "correctAnswer": "Especifica o destino do link.",
     "subject": "HTML"
@@ -101,32 +101,34 @@ const htmlQuestions = [
     "id": 9,
     "descricao": "O que é um elemento `<form>` em HTML?",
     "alternativas": [
+      "Um elemento de script para executar código JavaScript.",
       "Um contêiner para elementos de entrada de dados do usuário.",
-      "Um elemento de bloco para agrupar conteúdo.",
       "Um elemento de mídia para exibir imagens.",
-      "Um elemento de script para executar código JavaScript."
+      "Um elemento de bloco para agrupar conteúdo."
     ],
     "correctAnswer": "Um contêiner para elementos de entrada de dados do usuário.",
     "subject": "HTML"
   },
   {
     "id": 10,
-    "descricao": 'Qual é a função do atributo `action` em uma tag `<form>`?',
-    'alternativas': [
-      'Especifica a URL onde os dados do formulário serão enviados.',
-      'Define o método HTTP usado para enviar os dados.',
-      'Especifica o tipo de codificação dos dados.',
-      'Define o estilo do formulário.'
+    "descricao": "Qual é a função do atributo `action` em uma tag `<form>`?",
+    "alternativas": [
+      "Define o estilo do formulário.",
+      "Define o método HTTP usado para enviar os dados.",
+      "Especifica o tipo de codificação dos dados.",
+      "Especifica a URL onde os dados do formulário serão enviados."
     ],
+    "correctAnswer": "Especifica a URL onde os dados do formulário serão enviados.",
+    "subject": "HTML"
   },
   {
     "id": 11,
     "descricao": "Qual é a função do atributo `method` em uma tag `<form>`?",
     "alternativas": [
+      "Define o estilo do formulário.",
       "Define o método HTTP usado para enviar os dados.",
       "Especifica a URL onde os dados do formulário serão enviados.",
-      "Especifica o tipo de codificação dos dados.",
-      "Define o estilo do formulário."
+      "Especifica o tipo de codificação dos dados."
     ],
     "correctAnswer": "Define o método HTTP usado para enviar os dados.",
     "subject": "HTML"
@@ -135,10 +137,10 @@ const htmlQuestions = [
     "id": 12,
     "descricao": "O que é um elemento `<input>` em HTML?",
     "alternativas": [
-      "Um campo de entrada de dados do usuário.",
-      "Um elemento de bloco para agrupar conteúdo.",
       "Um elemento de mídia para exibir imagens.",
-      "Um elemento de script para executar código JavaScript."
+      "Um campo de entrada de dados do usuário.",
+      "Um elemento de script para executar código JavaScript.",
+      "Um elemento de bloco para agrupar conteúdo."
     ],
     "correctAnswer": "Um campo de entrada de dados do usuário.",
     "subject": "HTML"
@@ -147,10 +149,10 @@ const htmlQuestions = [
     "id": 13,
     "descricao": "Qual é a função do atributo `type` em uma tag `<input>`?",
     "alternativas": [
-      "Especifica o tipo de entrada de dados (texto, senha, etc.).",
       "Define o nome do campo de entrada.",
+      "Define o estilo do campo de entrada.",
       "Especifica o valor padrão do campo.",
-      "Define o estilo do campo de entrada."
+      "Especifica o tipo de entrada de dados (texto, senha, etc.)."
     ],
     "correctAnswer": "Especifica o tipo de entrada de dados (texto, senha, etc.).",
     "subject": "HTML"
@@ -159,34 +161,34 @@ const htmlQuestions = [
     "id": 14,
     "descricao": "O que é um elemento `<button>` em HTML?",
     "alternativas": [
-      "Um botão clicável para executar uma ação.",
+      "Um elemento de mídia para exibir imagens.",
       "Um campo de entrada de dados do usuário.",
-      "Um elemento de bloco para agrupar conteúdo.",
-      "Um elemento de mídia para exibir imagens."
+      "Um botão clicável para executar uma ação.",
+      "Um elemento de bloco para agrupar conteúdo."
     ],
     "correctAnswer": "Um botão clicável para executar uma ação.",
     "subject": "HTML"
   },
   {
     "id": 15,
-    "descricao": 'Qual é a função do atributo `onclick` em uma tag `<button>`?',
-    'alternativas': [
-      'Especifica a ação a ser executada quando o botão é clicado.',
-      'Define o texto exibido no botão.',
-      'Especifica o estilo do botão.',
-      'Define o tamanho do botão.'
+    "descricao": "Qual é a função do atributo `onclick` em uma tag `<button>`?",
+    "alternativas": [
+      "Define o tamanho do botão.",
+      "Define o texto exibido no botão.",
+      "Especifica a ação a ser executada quando o botão é clicado.",
+      "Especifica o estilo do botão."
     ],
-    "correctAnswer": 'Especifica a ação a ser executada quando o botão é clicado.',
+    "correctAnswer": "Especifica a ação a ser executada quando o botão é clicado.",
     "subject": "HTML"
   },
   {
     "id": 16,
     "descricao": "O que é um elemento `<table>` em HTML?",
     "alternativas": [
-      "Uma tabela para exibir dados em linhas e colunas.",
-      "Um elemento de bloco para agrupar conteúdo.",
       "Um campo de entrada de dados do usuário.",
-      "Um elemento de mídia para exibir imagens."
+      "Uma tabela para exibir dados em linhas e colunas.",
+      "Um elemento de mídia para exibir imagens.",
+      "Um elemento de bloco para agrupar conteúdo."
     ],
     "correctAnswer": "Uma tabela para exibir dados em linhas e colunas.",
     "subject": "HTML"
@@ -195,51 +197,50 @@ const htmlQuestions = [
     "id": 17,
     "descricao": "Qual é a função do elemento `<tr>` em uma tabela HTML?",
     "alternativas": [
-      "Define uma linha na tabela.",
-      "Define uma célula na tabela.",
+      "Define o rodapé da tabela.",
       "Define o cabeçalho da tabela.",
-      "Define o rodapé da tabela."
+      "Define uma linha na tabela.",
+      "Define uma célula na tabela."
     ],
     "correctAnswer": "Define uma linha na tabela.",
     "subject": "HTML"
   },
   {
     "id": 18,
-    "descricao": 'Qual é a função do elemento `<th>` em uma tabela HTML?',
-    'alternativas': [
-      'Define uma célula de cabeçalho na tabela.',
-      'Define uma célula de dados na tabela.',
-      'Define o rodapé da tabela.',
-      'Define o título da tabela.'
+    "descricao": "Qual é a função do elemento `<th>` em uma tabela HTML?",
+    "alternativas": [
+      "Define o título da tabela.",
+      "Define o rodapé da tabela.",
+      "Define uma célula de cabeçalho na tabela.",
+      "Define uma célula de dados na tabela."
     ],
-    "correctAnswer": 'Define uma célula de cabeçalho na tabela.',
+    "correctAnswer": "Define uma célula de cabeçalho na tabela.",
     "subject": "HTML"
   },
   {
     "id": 19,
-    "descricao": 'Qual é a função do elemento `<td>` em uma tabela HTML?',
-    'alternativas': [
-      'Define uma célula de dados na tabela.',
-      'Define uma célula de cabeçalho na tabela.',
-      'Define o rodapé da tabela.',
-      'Define o título da tabela.'
+    "descricao": "Qual é a função do elemento `<td>` em uma tabela HTML?",
+    "alternativas": [
+      "Define o rodapé da tabela.",
+      "Define uma célula de dados na tabela.",
+      "Define o título da tabela.",
+      "Define uma célula de cabeçalho na tabela."
     ],
-    "correctAnswer": 'Define uma célula de dados na tabela.',
+    "correctAnswer": "Define uma célula de dados na tabela.",
     "subject": "HTML"
   },
   {
     "id": 20,
-    "descricao": 'O que é um elemento `<iframe>` em HTML?',
-    'alternativas': [
-      'Um contêiner para incorporar outro documento HTML dentro do documento atual.',
-      'Um campo de entrada de dados do usuário.',
-      'Um elemento de bloco para agrupar conteúdo.',
-      'Um elemento de mídia para exibir vídeos.'
+    "descricao": "O que é um elemento `<iframe>` em HTML?",
+    "alternativas": [
+      "Um elemento de mídia para exibir vídeos.",
+      "Um elemento de bloco para agrupar conteúdo.",
+      "Um contêiner para incorporar outro documento HTML dentro do documento atual.",
+      "Um campo de entrada de dados do usuário."
     ],
-    "correctAnswer": 'Um contêiner para incorporar outro documento HTML dentro do documento atual.',
+    "correctAnswer": "Um contêiner para incorporar outro documento HTML dentro do documento atual.",
     "subject": "HTML"
   }
-
 ];
 
 module.exports = { htmlQuestions };
